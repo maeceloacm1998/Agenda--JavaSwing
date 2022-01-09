5b515bf9f283cbfd22c82d9f1707834d64fa2ba9
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-01-09 13:26:59 -0300

    Finish project

--- ui/MainForm.java
+++ ui/MainForm.java
@@ -4,6 +4,8 @@ import busisness.ContactBusissness;
 import entity.ContactEntity;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -19,13 +21,16 @@ public class MainForm extends JFrame {
 
     private ContactBusissness mContactBusissness;
 
+    private String name;
+    private String phone;
+
     public MainForm() {
         setContentPane(rootPainel);
-        setSize(500,250);
+        setSize(500, 250);
         setVisible(true);
 
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation(dim.width / 2 - getSize().width / 2,dim.height / 2 - getSize().height / 2);
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -35,7 +40,7 @@ public class MainForm extends JFrame {
         loadContacts();
     }
 
-    private void setListeners(){
+    private void setListeners() {
         buttonNewContact.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -44,29 +49,50 @@ public class MainForm extends JFrame {
             }
         });
 
+        tableContacts.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (e.getValueIsAdjusting()) {
+
+                    if (tableContacts.getSelectedRow() != -1) {
+                        name = tableContacts.getValueAt(tableContacts.getSelectedRow(), 0).toString();
+                        phone = tableContacts.getValueAt(tableContacts.getSelectedRow(), 1).toString();
+                    }
+
+                }
+            }
+        });
+
         buttonRemove.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                try {
+                    mContactBusissness.delete(name, phone);
+                    loadContacts();
 
-
+                    name = "";
+                    phone = "";
+                } catch (Exception ecpx) {
+                    JOptionPane.showMessageDialog(new JFrame(), ecpx.getMessage());
+                }
 
             }
         });
     }
 
-    private void loadContacts(){
-       List<ContactEntity> contactList =  mContactBusissness.getList();
+    private void loadContacts() {
+        List<ContactEntity> contactList = mContactBusissness.getList();
 
         String[] columNames = {"Nome", "Telefone"};
 
         DefaultTableModel model = new DefaultTableModel(new Object[0][0], columNames);
 
-        for(ContactEntity i : contactList){
+        for (ContactEntity i : contactList) {
             Object[] o = new Object[2];
 
-            o[0] =i.getName();
-            o[1] =i.getPhone();
+            o[0] = i.getName();
+            o[1] = i.getPhone();
 
             model.addRow(o);
         }
